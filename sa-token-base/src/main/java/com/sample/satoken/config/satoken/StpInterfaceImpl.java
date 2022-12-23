883e4f6f8b800bfd3c5f160e4419202d8a74480e
qlhan <qlhan@incarcloud.com>
2022-12-23 15:05:48 +0800

    feat: sa-token cache
    permission+role cache

--- sa-token-base/src/main/java/com/sample/satoken/config/satoken/StpInterfaceImpl.java
+++ sa-token-base/src/main/java/com/sample/satoken/config/satoken/StpInterfaceImpl.java
@@ -1,7 +1,6 @@
 package com.sample.satoken.config.satoken;
 
 import cn.dev33.satoken.session.SaSession;
-import cn.dev33.satoken.session.SaSessionCustomUtil;
 import cn.dev33.satoken.stp.StpInterface;
 import cn.dev33.satoken.stp.StpUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +23,8 @@ public class StpInterfaceImpl implements StpInterface {
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         List<String> pList = new ArrayList<>();
-        SaSession session = SaSessionCustomUtil.getSessionById(StpUtil.getLoginIdAsString());
+        //SaSession session = SaSessionCustomUtil.getSessionById(StpUtil.getLoginIdAsString());
+        SaSession session = StpUtil.getSession();
         switch (String.valueOf(loginId)) {
             case "10001":
                 List<String> list = session.get("permission-list", () -> Arrays.asList("action.delete", "action.page", "action.all"));
@@ -41,7 +41,8 @@ public class StpInterfaceImpl implements StpInterface {
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
         List<String> rList = new ArrayList<>();
-        SaSession session = SaSessionCustomUtil.getSessionById(StpUtil.getLoginIdAsString());
+        //SaSession session = SaSessionCustomUtil.getSessionById(StpUtil.getLoginIdAsString());
+        SaSession session = StpUtil.getSession();
         switch (String.valueOf(loginId)) {
             case "10001":
                 log.info("getRoleList -> loginId=10001, rList={}", rList);
